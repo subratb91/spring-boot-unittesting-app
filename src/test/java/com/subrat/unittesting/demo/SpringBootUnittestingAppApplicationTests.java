b25de7963a95b6aecf7474f7e6dc4cde7633a1d2
Subrat Behera <skb@outlook.in>
2019-11-30 11:56:56 +0530

    Creating different test configurations using @TestPropertySource

--- src/test/java/com/subrat/unittesting/demo/SpringBootUnittestingAppApplicationTests.java
+++ src/test/java/com/subrat/unittesting/demo/SpringBootUnittestingAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.subrat.unittesting.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+//@TestPropertySource(locations = { "classpath:test-configuration.properties" })
 class SpringBootUnittestingAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
